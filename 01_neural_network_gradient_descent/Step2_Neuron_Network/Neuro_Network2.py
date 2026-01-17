1c8dfe247219b859f789df096f9fe35bc113b0eb
Thomas-robotique <tmoscheni78@gmail.com>
2026-01-17 16:19:12 +0100

    Create Neuro_Network2.py

---
+++ 01_neural_network_gradient_descent/Step2_Neuron_Network/Neuro_Network2.py
@@ -0,0 +1,286 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.metrics import accuracy_score
+from tqdm import tqdm
+from matplotlib.animation import FuncAnimation
+
+
+# Création de la base de données d'entraînement fictive
+
+'''
+# Génère un jeu de données artificiel composé de 200 points en 2D, 
+# # répartis en 2 groupes, avec une graine aléatoire fixée pour 
+# obtenir toujours les mêmes données.
+X, y = make_blobs(n_samples=200, n_features=2, centers=2, random_state=0) 
+y = y.reshape((y.shape[0], 1))
+
+
+
+
+plt.scatter(X[:,0], X[:, 1], c=y, cmap='summer')
+X = X.T
+y = y.reshape(1, -1)
+'''
+def make_two_concentric_circles(n_points=600, noise=0.05, r1=2.0, r2=4.0):
+
+
+    
+
+   n = n_points // 2
+   theta = np.sqrt(np.random.rand(n)) * 2 * np.pi
+
+   r_a = 2 * theta + np.pi
+   data_a = np.array([np.cos(theta) * r_a, np.sin(theta) * r_a]).T
+   data_a += np.random.randn(n, 2) * noise
+
+   r_b = -2 * theta - np.pi
+   data_b = np.array([np.cos(theta) * r_b, np.sin(theta) * r_b]).T
+   data_b += np.random.randn(n, 2) * noise
+
+   X = np.vstack([data_a, data_b])
+   y = np.hstack([np.zeros(n), np.ones(n)])
+
+   return X.T, y.reshape(1, -1)
+
+
+
+X, y = make_two_concentric_circles()
+
+
+
+plt.scatter(X[0,:], X[1,:], c=y.flatten(), cmap='summer')
+plt.xlabel("X1")
+plt.ylabel("X2")
+plt.title("Cercle concentriques")
+plt.show()
+
+
+# Fonction d’initialisation qui permet d’initialiser les paramètres w et b
+def Initialisation(X,NC0, NC1, NC2):                 #NC0 = Nombre d'entrées               
+    b1 = np.random.randn(NC1,1)                       #NC2= Nombre de neurones couche2 qui doit être fixé à 1 pour avoir une sortie binaire
+
+
+    w1 = np.random.randn(NC1, X.shape[0]) * 0.1         # np.random.randn et pas rand pour éviter d'avoir des poids trop petits
+    w2 = np.random.randn(NC2, NC1) * 0.1
+
+    b2=np.random.randn(NC2,1)
+
+    parametre = {
+            "w1": w1,
+            "b1": b1,
+            "w2":w2,
+            "b2":b2
+
+    }
+
+    return parametre
+
+# Fonction modèle : calcule la prédiction grâce à la fonction sigmoïde
+# et prédit la position du point sur le graphe
+def model(X, parametre):
+    w1=parametre["w1"]
+    b1=parametre["b1"]
+    w2=parametre["w2"]
+    b2=parametre["b2"]
+
+     
+
+    z1 = w1.dot(X) + b1 # Broadcasting : NumPy étire b1 pour qu'il corresponde aux dimensions de w1
+
+
+    A1 = 1 / (1 + np.exp(-z1))  
+
+   
+
+  
+
+    z2= w2.dot(A1)+b2
+    A2= 1/(1+np.exp(-z2))
+
+    activation ={          #Tableau qui contient les activations des neurones
+
+        "z1": z1,
+      
+        "A1": A1,
+        "A2":A2,
+        "z2":z2
+       
+    }
+   
+    return activation
+
+# Fonction log_loss qui calcule à quel point la prédiction est proche de la réalité
+def Logg_loss(activation, y):
+    epsilon= pow(10,-15)
+    m=y.shape[1]         # m correspond au nombre de données
+
+    A2= activation["A2"]
+
+    return 1 / m * np.sum(-y * np.log(A2+epsilon) - (1 - y) * np.log(1 - A2+epsilon))
+
+# Fonction qui calcule les dérivées partielles de la fonction log_loss
+# par rapport à w et b
+def gradiant(y, X, parametre, activation):
+    
+     w1=parametre["w1"]    
+     w2= parametre["w2"]
+     b1=parametre["b1"]
+     b2=parametre["b2"]
+
+
+     A1= activation["A1"]
+     A2=activation["A2"]
+     z1=activation["z1"]
+     z2=activation["z2"]
+
+
+     dz2=A2-y                                           # Formules mathématiques des dérivées partielles
+
+     m=y.shape[1]
+  
+     dw2 = 1 / m * dz2.dot(A1.T)
+     db2 = 1 / m * np.sum(A2 - y, axis=1, keepdims=True)
+
+
+     dz1=np.dot(w2.T, dz2)*A1*(1-A1)
+     db1 = 1 / m * np.sum(dz1, axis=1, keepdims=True)
+     dw1=1 / m * dz1.dot(X.T)
+
+     gradiant = {
+          "dw1":dw1,
+          "dw2":dw2,
+          "db1":db1,
+          "db2": db2
+        }
+
+        
+     
+
+     return gradiant
+    
+        
+
+# Fonction qui ajuste les paramètres w et b en fonction de la fonction log_loss
+def uptade(a,gradiant,parametre ):
+
+ dw1=gradiant["dw1"]
+ dw2=gradiant["dw2"]
+ db1=gradiant["db1"]
+ db2=gradiant["db2"]
+
+ w1=parametre["w1"]
+ w2=parametre["w2"] 
+ b1=parametre["b1"]
+ b2=parametre["b2"]
+
+ w1 = w1 - a * dw1
+ b1 = b1 - a * db1
+
+ w2 = w2 - a * dw2
+ b2 = b2 - a * db2
+
+ parametre ={
+     "w1":w1,
+     "w2":w2,
+     "b1":b1,
+     "b2":b2
+   
+ }
+ return  parametre
+
+
+# Fonction de prédiction : renvoie True (1) si la probabilité >= 0.5, sinon False (0)
+def predict(Y, parametre):
+    activation = model(Y, parametre)
+    A2= activation["A2"]
+    return A2>=0.5
+
+# Fonction finale qui combine toutes les fonctions précédentes dans une boucle avec un nombre d’itérations fixé. Plus ce nombre est grand, plus le modèle s’entraîne longtemps.
+def Neuron_network(X, y, a=0.1, n_inter=3000):
+    NC0= X.shape[0]
+    NC1=300
+    NC2=1
+    parametre = Initialisation(X,NC0, NC1, NC2)
+    w1= parametre["w1"]
+    w2=parametre["w2"]
+    b1=parametre["b1"]
+    b2=parametre["b2"]
+    loss = []
+    acc =[]
+    for i in tqdm(range(n_inter)):
+        activation = model(X, parametre)
+        loss.append(Logg_loss(activation, y))
+        grad = gradiant(y, X, parametre, activation)
+        parametre = uptade(a,grad, parametre)
+        y_pred= predict(X,parametre)
+        acc.append(accuracy_score(y.flatten(), y_pred.flatten()))  # Calcule le taux de précision entre les vraies étiquettes (y) et les prédictions (y_pred)
+        if i % 10 == 0:   # Sauvegarde toutes les 10 itérations
+         
+         history.append({
+         "w1": parametre["w1"].copy(),
+         "b1": parametre["b1"].copy(),
+         "w2": parametre["w2"].copy(),
+         "b2": parametre["b2"].copy()
+    })
+         
+         
+
+
+ 
+    
+        
+    # Affichage de la fonction log_loss
+    plt.figure(figsize=(8, 5))
+    plt.plot(loss, color='blue', linewidth=2)
+    plt.title("Évolution du Log Loss pendant l'apprentissage", fontsize=14)
+    plt.xlabel("Itérations", fontsize=12)
+    plt.ylabel("Log Loss", fontsize=12)
+    plt.grid(True)
+    plt.plot(acc)  # Affiche la courbe d'accuracy sur le graphe, utile pour repérer un éventuel surapprentissage (overfitting)
+
+    plt.show()
+    return parametre, history
+    
+
+
+
+def plot_decision_boundary(ax, params):
+    w1, b1 = params["w1"], params["b1"]
+    w2, b2 = params["w2"], params["b2"]
+
+    x0 = np.linspace(X[0,:].min()-1, X[0,:].max()+1, 200)
+    x1 = np.linspace(X[1,:].min()-1, X[1,:].max()+1, 200)
+    xx, yy = np.meshgrid(x0, x1)
+    grid = np.vstack((xx.ravel(), yy.ravel()))
+
+    z1 = w1.dot(grid) + b1
+    A1 = 1/(1+np.exp(-z1))
+    z2 = w2.dot(A1) + b2
+    A2 = 1/(1+np.exp(-z2))
+
+    Z = A2.reshape(xx.shape)
+    ax.contourf(xx, yy, Z, levels=[0,0.5,1], alpha=0.3, cmap="RdYlGn")
+
+## fig → la figure complète, c’est-à-dire la “page” ou la “fenêtre” où tout sera affiché.
+## ax → l’axe, c’est-à-dire la zone de dessin dans la figure. C’est là que tu traces les courbes, les points, les contours.
+
+history=[]
+
+parametre, history = Neuron_network(X, y)
+
+
+fig, ax = plt.subplots() ## On créer une figure et un axe pour dessiner 
+
+   
+def animate(i):
+    ax.clear()
+    ax.scatter(X[0,:], X[1,:], c=y.flatten(), cmap='summer')
+    plot_decision_boundary(ax, history[i])
+    ax.set_title(f"Iteration {i}")
+
+
+
+ani = FuncAnimation(fig, animate, frames=len(history), interval=50)
+plt.show()
+ 
